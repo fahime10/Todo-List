0e441cd58bc4d27ee848657dbf98303ae7c4f1d3
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-08 00:21:22 +0100

    Implemented view todos functionality

--- src/index.js
+++ src/index.js
@@ -2,34 +2,73 @@ import './style.css';
 import { Project } from './modules/project.js';
 import { Todo } from './modules/todo.js';
 
+let projects = [];
+
 function loadProjects() {
     const projectsContainer = document.querySelector('#project-container');
-    const firstProject = new Project("First Project");
+    const firstProject = new Project(1, "First Project");
+    const secondProject = new Project(2, "Second Project");
 
     const projectDiv = document.createElement('div');
-    projectDiv.textContent = firstProject.name;
     projectDiv.classList.add('project');
+    projectDiv.append(firstProject.name);
+    projectDiv.id = firstProject.id;
 
-    const todo = new Todo("Title", "Description", "06/06/2023", "High", "No");
+    const projectDiv2 = document.createElement('div');
+    projectDiv2.classList.add('project');
+    projectDiv2.append(secondProject.name);
+    projectDiv2.id = secondProject.id;
+
+    projects.push(firstProject, secondProject);
+
+    const todo = new Todo("First todo", "Description normal", "06/06/2023", "High", "No");
+    const todo2 = new Todo("Second todo", "Description new", "06/06/2023", "High", "No");
+    const todo3 = new Todo("Third todo", "Description normal", "06/06/2023", "High", "No");
+    const todo4 = new Todo("Fourth todo", "Description new", "06/06/2023", "High", "No");
+    const todo5 = new Todo("Fifth todo", "Description normal", "06/06/2023", "High", "No");
+    const todo6 = new Todo("Sixth todo", "Description new", "06/06/2023", "High", "No");
 
     firstProject.addTodo(todo);
+    firstProject.addTodo(todo2);
+    secondProject.addTodo(todo3);
+    secondProject.addTodo(todo4);
+    secondProject.addTodo(todo5);
+    secondProject.addTodo(todo6);
 
-    projectsContainer.appendChild(projectDiv);
+    projectsContainer.append(projectDiv);
+    projectsContainer.appendChild(projectDiv2);
 
-    const projects = document.querySelectorAll('.project');
-    projects.forEach((project) => {
+    const allProjects = document.querySelectorAll('.project');
+    allProjects.forEach((project) => {
         project.addEventListener("click", () => {
-            projects.forEach((project) => {
+            allProjects.forEach((project) => {
                 project.style.backgroundColor = "rgb(231, 224, 224)";
             });
             project.style.backgroundColor = "rgb(75, 143, 232)";
+            loadTodos(projects[project.id - 1]);
         });
     });
-
-    return projectsContainer;
 }
+loadProjects();
 
-document.body.appendChild(loadProjects());
+function loadTodos(projectSelected) {
+    const todosContainer = document.querySelector('#todos-container');
+    todosContainer.innerHTML = "";
+
+    const projects = document.querySelectorAll('.project');
+    projects.forEach((project) => {
+        if (project.style.backgroundColor == "rgb(75, 143, 232)") {
+            const todos = projectSelected.viewTodos();
+            todos.forEach((todo) => {
+                const todoDiv = document.createElement('div');
+                todoDiv.innerHTML = projectSelected.stringFormat(todo);
+                todoDiv.classList.add('todo');
+
+                todosContainer.appendChild(todoDiv);
+            });
+        }
+    });
+}
 
 function displayAddForm() {
     document.querySelectorAll('.project').forEach((project) => {
@@ -43,15 +82,16 @@ function submitProject(event) {
     event.preventDefault();
     const projectName = document.querySelector('#project-name');
     if (validateProjectName(projectName.value)) {
-        const newProject = new Project(`${projectName.value}`);
+        const newProject = new Project(3, `${projectName.value}`);
         const projectsContainer = document.querySelector('#project-container');
         const newProjectDiv = document.createElement('div');
         newProjectDiv.textContent = newProject.name;
         newProjectDiv.classList.add('project');
+        newProjectDiv.id = newProject.id;
         projectsContainer.appendChild(newProjectDiv);
 
         document.querySelectorAll('.project').forEach((project) => {
-            project.style.display = "block";
+            project.style.display = "flex";
         });
 
         document.querySelector('#form-project').style.display = "none";
@@ -76,3 +116,12 @@ function cancelProject(event) {
     });
 }
 document.querySelector('#cancel-project').addEventListener('click', cancelProject);
+
+function editSelectedProject() {
+    const projects = document.querySelectorAll('.project');
+    projects.forEach((project) => {
+        if (project.style.backgroundColor == "rgb(75, 143, 232)") {
+
+        }
+    });
+}
